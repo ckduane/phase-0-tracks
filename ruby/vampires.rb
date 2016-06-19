bf7d4d7b2ac47f0e4a406b3d8a2fc33c14a11989
christine <ckduane@gmail.com>
2016-06-18 19:53:44 -0700

    completed release 3

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,65 +1,73 @@
-puts "What is your name?"
-name = gets.chomp
+puts "How many employees will be processed?"
+employees = gets.chomp.to_i
+survey = 0
 
-	if (name == "Drake Cula" || name == "Tu Fang")
-		name = true
-	else
-		name = false
-	end
+until employees == survey
+	survey += 1
 	
-puts name
-
-puts "How old old are you?"
-stated_age = gets.chomp.to_i
-
-puts "What year were you born?"
-year_born = gets.chomp.to_i
-
-actual_age = 2016 - year_born
-
-puts stated_age
-puts actual_age
-
-	if stated_age == actual_age
-		age = false
+	puts "What is your name?"
+	name = gets.chomp
+	
+		if (name == "Drake Cula" || name == "Tu Fang")
+			name = true
+		else
+			name = false
+		end
+		
+	puts name
+	
+	puts "How old old are you?"
+	stated_age = gets.chomp.to_i
+	
+	puts "What year were you born?"
+	year_born = gets.chomp.to_i
+	
+	actual_age = 2016 - year_born
+	
+	puts stated_age
+	puts actual_age
+	
+		if stated_age == actual_age
+			age = false
+		else
+			age = true
+		end
+	
+	puts age
+	
+	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	garlic = gets.chomp
+	
+		if garlic == "no"
+			garlic = true
+		else
+			garlic = false
+		end
+		
+	puts garlic
+	
+	puts "Would you like to enroll in the company’s health insurance?"
+	insurance = gets.chomp
+		
+		if insurance == "no"
+			insurance = true
+		else
+			insurance = false
+		end
+		
+	puts insurance
+	
+	
+	if name
+		puts "Definitely a vampire"
+	elsif !age && !(garlic || insurance)
+		puts "Probably not a vampire"
+	elsif age && (garlic || insurance)
+		puts "Probably a vampire"
+	elsif age && (garlic && insurance)
+		puts "Almost certainly a vampire"
 	else
-		age = true
+		puts "Results inconclusive"
 	end
 
-puts age
-
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic = gets.chomp
-
-	if garlic == "no"
-		garlic = true
-	else
-		garlic = false
-	end
-	
-puts garlic
-
-puts "Would you like to enroll in the company’s health insurance?"
-insurance = gets.chomp
-	
-	if insurance == "no"
-		insurance = true
-	else
-		insurance = false
-	end
-	
-puts insurance
-
-
-if name
-	puts "Definitely a vampire"
-elsif !age && !(garlic || insurance)
-	puts "Probably not a vampire"
-elsif age && (garlic || insurance)
-	puts "Probably a vampire"
-elsif age && (garlic && insurance)
-	puts "Almost certainly a vampire"
-else
-	puts "Results inconclusive"
 end
-
