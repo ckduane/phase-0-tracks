748482a87e7ee35b13a735102f8dd57aa8ba7d75
christine <ckduane@gmail.com>
2016-06-28 19:22:59 -0700

    finished release 2

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -8,22 +8,66 @@
   # use interpolation to make things readable
 # output: hash
 
-# Method to add an item to a list
-# input: item name and optional quantity
-# steps: check to see if item is in the hash. if so, use update method. if no, add to hash
-# output: updated hash
+$grocery_list = {}
 
-# Method to remove an item from the list
-# input: item name 
-# steps: check if item is in the hash. if so, remove it
-# output: updated hash
+def create(item)
+	item_word = item.split(' ')
+	item_word.map { |k, v| $grocery_list[k.to_sym] = 1 }
+	puts "Thank you for creating a grocery list. Here is your list"
+	$grocery_list.each { |k, v| puts "#{k}: #{v}" }
+end
 
-# Method to update the quantity of an item
-# input: item name and quantity of item which would be required
-# steps: look up item. change quantity
-# output: updated hash
+create("carrot radish orange")
 
 # Method to print a list and make it look pretty
 # input: hash
 # steps: iterating through hash with .each
 # output: list of grocery items and their quantity in a sting
+
+def pretty_list
+	$grocery_list.each { |k, v| puts "#{k}: #{v}" }
+end
+
+pretty_list
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: check to see if item is in the hash. if no, add to hash
+# output: updated hash
+
+def add(item, quantity = 1)
+	$grocery_list[item.to_sym] = quantity
+	puts "#{quantity} #{item} added"
+	pretty_list
+end
+
+add("apple", 2)
+
+
+# Method to remove an item from the list
+# input: item name 
+# steps: remove item if listed
+# output: updated hash
+
+def remove(item)
+	$grocery_list.delete(item.to_sym)
+	puts "#{item} has been removed! "
+	puts "Here is your updated grocery list:"
+	pretty_list	
+end
+
+remove("carrot")
+
+# Method to update the quantity of an item
+# input: item name and quantity of item which would be required
+# steps: change quantity
+# output: updated hash
+
+def update(item, quantity)
+	$grocery_list[item.to_sym] = quantity
+	puts "#{quantity} #{item} updated"
+	pretty_list
+end
+
+add("apple", 5)
+
